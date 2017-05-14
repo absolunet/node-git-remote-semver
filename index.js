78cf77a90cd652afb6a6e14486c0c0d7f6d9786d
Jb Landry <jb@jblandry.info>
2017-05-14 19:22:49 -0400

    Added updateNotification

--- index.js
+++ index.js
@@ -4,8 +4,18 @@
 'use strict';
 
 const { exec } = require('child_process');
+const boxen    = require('boxen');
+const chalk    = require('chalk');
 const semver   = require('semver');
 const toSemver = require('to-semver');
+
+
+const DICTIONARY = {
+	updateAvailable: {
+		fr: 'Mise à jour disponible',
+		en: 'Update available'
+	}
+};
 
 
 
@@ -33,7 +43,7 @@ module.exports = class GitRemoteSemver {
 	static getLatest(url) {
 		return new Promise((resolve) => {
 			this.getVersions(url).then((versions) => {
-				resolve(versions.shift());
+				resolve(versions.shift() || null);
 			});
 		});
 	}
@@ -41,9 +51,23 @@ module.exports = class GitRemoteSemver {
 	static needUpdate(url, current) {
 		return new Promise((resolve) => {
 			this.getLatest(url).then((latest) => {
-				resolve(semver.gt(latest, current) ? latest : false);
+				resolve(latest && semver.gt(latest, current) ? latest : false);
 			});
 		});
+	}
+
+
+	// Simulate npm's update-notifier behavior
+	static updateNotification({ current, latest, lang = 'en', msg }) {
+		return boxen(
+			`${DICTIONARY.updateAvailable[lang]} ${chalk.dim(current)} ${chalk.reset('→')} ${chalk.green(latest)}${msg ? `\n${msg}` : ''}`,
+			{
+				padding:     1,
+				margin:      0.5,
+				align:       'left',
+				borderColor: 'yellow'
+			}
+		);
 	}
 
 };
